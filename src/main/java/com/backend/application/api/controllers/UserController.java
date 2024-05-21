67edbfca1ec7fe5f14fd41c25fb4df784faa76c6
Alif Gunadi <alifgunadi1303@gmail.com>
2024-05-21 10:31:39 +0700

    update: add encrypt password

--- src/main/java/com/backend/application/api/controllers/UserController.java
+++ src/main/java/com/backend/application/api/controllers/UserController.java
@@ -1,9 +1,12 @@
 package com.backend.application.api.controllers;
 
 import java.util.List;
+import java.util.Date;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +22,7 @@ public class UserController {
 
     @Autowired
     public UserRepo userRepo;
+    private final BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
 
     @GetMapping(value = "/list/users")
     public ResponseEntity<List<User>> getUsers() {
@@ -30,7 +34,6 @@ public class UserController {
         }
 
         return ResponseEntity.ok(users);
-
     }
 
     @GetMapping(value = "/detail/users/{id}")
@@ -42,15 +45,47 @@ public class UserController {
         userOptional.setPassword(null);
 
         return ResponseEntity.ok(userOptional);
-
     }
 
     @PostMapping(value = "/register/user")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
+        String encryptPassword = bcryptPasswordEncoder.encode(user.getPassword());
+
+        user.setPassword(encryptPassword);
+
         userRepo.save(user);
 
         return ResponseEntity.ok("Register is successfully.");
-        
     }
 
+    @PostMapping(value = "/update/user")
+    public ResponseEntity<String> updateUser(@RequestBody User user) {
+        long id = user.getId();
+        User userId = userRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User id not found:" + id));
+
+        if (user.getPassword() != "" && user.getPassword() != null && !user.getPassword().isEmpty()) {
+            String encryptPassword = bcryptPasswordEncoder.encode(user.getPassword());
+            userId.setPassword(encryptPassword);
+        }
+
+        userId.setUsername(user.getUsername());
+        userId.setEmail(user.getEmail());
+        userId.setPrivilegeId(user.getPrivilegeId());
+        userId.setUpdatedAt(new Date());
+
+        userRepo.save(userId);
+
+        return ResponseEntity.ok("Update user successfully.");
+    }
+
+    @DeleteMapping(value = "delete/user/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable long id) {
+        User userDeleted = userRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User id not found" + id));
+
+        userRepo.delete(userDeleted);
+
+        return ResponseEntity.ok("Delete user id: " + id + " successfully.");
+    }
 }
